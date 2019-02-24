752daa187a7988e74ca54c83550abb9f1ca7cfd7
Ben Nelson <bgnelson25@gmail.com>
2019-02-24 14:38:32 -0600

    Added bootstrap and css
    Added bootsrap to python. Moved css file and added basic css.

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
